7699f0f589417fe6cf96d28d96ecfff49113f7a2
Pascual Galvan <josgalbar@alu.edu.gva.es>
2023-11-26 20:04:28 +0100

    Version 0.1

---
+++ src/ut5.java
@@ -0,0 +1,16 @@
+package ut5_pascual_galvan;
+import javax.swing.JFrame; // importamos marco
+import javax.swing.JLabel; // importamos etiqueta
+public class ut5 {
+
+           public static void main (String[]args){
+            // nombre del marco asiganos objeto frame a ventana
+            JFrame ventana = new JFrame("Unidad 5");
+            ventana.setSize(300, 300); // tamaño marco
+            ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JLabel etiqueta = new JLabel("Hola Mundo"); // nombre etiqueta
+            ventana.add(etiqueta); // añadimos la etiqueta al marco
+            ventana.setVisible(true); // hacemos visible el marco
+           }
+}
